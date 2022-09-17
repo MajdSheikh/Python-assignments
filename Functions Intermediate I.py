5755597eb0a3ae20b08349e52d5f8b605257ff7e
Majd Sheikh <majd_sheikh@hotmail.com>
2022-09-17 16:57:10 +0300

    Functions Intermediate I

---
+++ Functions Intermediate I.py
@@ -0,0 +1,27 @@
+import random
+# random integer between 0 and 100
+def randInt():
+    print(int(random.random()*100))
+randInt()
+
+
+# here the max number is 50
+def randInt():
+    print(int(random.random()*50))
+randInt()
+
+
+#random integer between 50 and 100
+def randInt():
+    print(int(random.random() * 50 + 50))
+randInt()
+
+#random integer between 50 and 500
+def randInt():
+    print(int(random.random() * 450 + 50))
+randInt()
+
+
+
+
+
